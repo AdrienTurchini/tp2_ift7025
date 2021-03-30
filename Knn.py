94fb8b0d01bf6cc2e0a95e9d0eb5c06bdfe0aaa0
peopleOfPalay <marck.henriot@gmail.com>
2021-03-30 19:06:40 +0200

    Knn_v1

--- Knn.py
+++ Knn.py
@@ -10,6 +10,12 @@ class Knn:
         """
         self.K = K
 
+    def euclidean_distance(self, x1, x2):
+        distance = 0
+        for i in range(len(x1) - 1):
+            distance += (x1[i] - x2[i])**2
+        return distance**0.5
+
     def train(self, train, train_labels):  # vous pouvez rajouter d'autres attributs au besoin
         """
         C'est la méthode qui va entrainer votre modèle,
@@ -21,45 +27,77 @@ class Knn:
 
         vous pouvez rajouter d'autres arguments, il suffit juste de
         les expliquer en commentaire
-
         """
+        assert len(train) == len(train_labels)
+        self.X_train = train
+        self.y_train = train_labels
 
-    def predict(self, x):
+    def getNeighbors(self, X_test_row):
+        '''
+        Description:
+            Calcul les distances avec X_test_row
+        Output:
+            k-nearest neighbors to the test data
+        '''
+        distances = []
+        for i, X_train_row in enumerate(self.X_train):
+            distances.append([X_train_row, self.euclidean_distance(X_test_row, X_train_row), self.y_train[i]])
+        distances.sort(key=lambda distances: distances[1])
+
+        neighbors = []
+        for i in range(self.K):
+            neighbors.append(distances[i])
+        return neighbors
+
+
+    def predict(self, X):
         """
         Prédire la classe d'un exemple x donné en entrée
         exemple est de taille 1xm
         """
-        return np.array([])
+        y_pred = []
+        
+        for X_test_row in X:
+            K_neighbors = self.getNeighbors(X_test_row)
+            classes = [row[-1] for row in K_neighbors]
+            prediction = max(set(classes), key=classes.count)
+            y_pred.append(prediction)
+        
+        return np.array(y_pred)
 
     def accuracy(self):
         """
+        Calcul l'accuracy avec :
         ACC = (TP + TN) / (TP + TN + FP + FN)
         """
         return (self.TP + self.TN) / (self.TP + self.TN + self.FP + self.FN)
 
     def precision(self):
         """
+        Calcul la precision avec :
         PPV = (TP) / (TP + FP)
         """
         return (self.TP) / (self.TP + self.FP)
 
     def recall(self):
         """
+        Calcul le recall avec :
         TPR = (TP) / (TP + FN)
         """
         return (self.TP) / (self.TP + self.FN)
 
     def f1_score(self):
         """
+        Calcul le f1_score avec :
         ACC = 2 * (PPV * TPR) / (PPV + TPR)
         """
         return (2 * self.precision() * self.recall()) / (self.precision() + self.recall())
 
     def confusion_matrix(self):
         """
-        Compute the confusion matrix
+        retourne la matrice de confusion
         """
-        return np.array([self.TP, self.FP], [self.FN, self.TN])
+        return np.array([[self.TP, self.FP], [self.FN, self.TN]])
 
     def evaluate(self, X, y):
         """
@@ -77,16 +115,12 @@ class Knn:
 
         self.TP = np.sum(y[y==1] == y_pred[y==1])
         self.TN = np.sum(y[y==0] == y_pred[y==0])
-        self.FN = np.sum(y[y==1] != y_pred[y==1])
         self.FP = np.sum(y[y==0] != y_pred[y==0])
+        self.FN = np.sum(y[y==1] != y_pred[y==1])
 
         return {'Accuracy': self.accuracy(), 
-                  'Precision': self.precision(), 
-                  'Recall': self.recall(), 
-                  'F1-score': self.f1_score(), 
-                  'Confusion_matrix': self.confusion_matrix()
-                  }
-
-
-    # Vous pouvez rajouter d'autres méthodes et fonctions,
-    # il suffit juste de les commenter.
+                'Precision': self.precision(), 
+                'Recall': self.recall(), 
+                'F1-score': self.f1_score(), 
+                'Confusion_matrix': self.confusion_matrix()
+                }
